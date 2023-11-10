f86b194fc5d5a02798707226fa9924f40e75e5b9
Holakarma <holakarmos@gmail.com>
2023-11-10 17:43:05 +0500

    index html

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import mobileNav from './modules/mobile-nav.js';
+import cursor from './modules/cursor.js';
+import swiperComments from './modules/swiper-comments.js';
+import swiperTeam from './modules/swiper-team.js';
+import swiperWeProud from './modules/swiper-we-proud.js';
+
+swiperWeProud();
+swiperTeam();
+swiperComments();
 mobileNav();
+cursor();

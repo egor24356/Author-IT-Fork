6d94f61f20c7ecd5cd1b063c25b5c29348c4cb8b
Holakarma <holakarmos@gmail.com>
2023-11-28 18:01:26 +0500

    modified footer, fixed casesFilter

--- src/js/aboutus.js
+++ src/js/aboutus.js
@@ -1,5 +1,7 @@
 import mobileNav from './modules/mobile-nav.js';
 import aboutus from './modules/AboutUs.js';
+import headerLinks from './modules/header-links.js';
 
+headerLinks();
 mobileNav();
 aboutus();

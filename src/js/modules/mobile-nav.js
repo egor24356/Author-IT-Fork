3ed2654d23c56fdbfeffa16f0338937e428efd40
Holakarma <holakarmos@gmail.com>
2023-12-04 20:01:20 +0500

    started rework mobile-nav

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -3,9 +3,12 @@ function mobileNav() {
 	const navBtn = document.querySelector('.mobile-nav-btn');
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
+	const navCircle = document.querySelector('.mobile-nav-circle')
 
 	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
+		// nav.classList.toggle('mobile-nav--open');
+		// nav.classList.toggle('visually-hidden');
+		navCircle.classList.toggle('mobile-nav-circle--active');
 		menuIcon.classList.toggle('nav-icon--active');
 		document.body.classList.toggle('no-scroll');
 	};

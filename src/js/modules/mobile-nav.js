3735faac26852c36934d19f2f685729e92c29f3d
Holakarma <holakarmos@gmail.com>
2023-12-05 18:02:31 +0500

    reworked mobile-nav

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -4,13 +4,28 @@ function mobileNav() {
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
 	const navCircle = document.querySelector('.mobile-nav-circle')
+	const html = document.querySelector('html');
+
+	function changeIcon() {
+		menuIcon.classList.toggle('nav-icon--active');
+	}
 
 	navBtn.onclick = function () {
-		// nav.classList.toggle('mobile-nav--open');
+		nav.classList.toggle('mobile-nav--open');
 		// nav.classList.toggle('visually-hidden');
 		navCircle.classList.toggle('mobile-nav-circle--active');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
+		if (menuIcon.classList.contains('nav-icon--active')) {
+			setTimeout(changeIcon, 350);
+			html.style.overflow = 'auto';
+			document.body.style.height = 'auto';
+			document.body.classList.remove('no-scroll');
+
+		} else {
+			menuIcon.classList.toggle('nav-icon--active');
+			html.style.overflow = 'hidden';
+			document.body.style.height = '100vh';
+			document.body.classList.add('no-scroll');
+		}
 	};
 }
 

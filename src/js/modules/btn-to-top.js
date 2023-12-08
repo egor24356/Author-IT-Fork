7e1f54dfcff236b5cf26989d888352d4ed9c6b7f
Holakarma <holakarmos@gmail.com>
2023-12-08 16:53:46 +0500

    modified btn-up button

--- src/js/modules/btn-to-top.js
+++ src/js/modules/btn-to-top.js
@@ -1,24 +1,21 @@
 function btnToTop () {
-    function action() {
-        const form = document.querySelector('.b24-form');
-        if (!form) return
+    const footer = document.querySelector('.footer');
+    if (!footer) return
 
-        const btn = document.querySelector('.to-top');
+    const btn = document.querySelector('.to-top');
+    let scroll;
+    let footerPos;
+
+    window.addEventListener('scroll', function() {
         
-        let scroll;
-        let formPos;
-        
-        window.addEventListener('scroll', function() {
-            scroll = window.scrollY;
-            formPos = form.getBoundingClientRect().top + scroll;
-            if (scroll > 1500 && scroll < formPos - 500) {
-                btn.classList.add('to-top--active');
-            } else {
-                btn.classList.remove('to-top--active');
-            }
-        })
-    }
-    setTimeout(action, 1000);
+        scroll = window.scrollY;
+        footerPos = footer.getBoundingClientRect().top + scroll;
+        if (scroll > 1500 && scroll < footerPos - 1000) {
+            btn.classList.add('to-top--active');
+        } else {
+            btn.classList.remove('to-top--active');
+        }
+    })
 }
 
 export default btnToTop;
